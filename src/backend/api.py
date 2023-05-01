43bda07f2cac0db312b31830820ceae33ed564a4
Jan-Matter <jan.matter@imnoo.com>
2023-05-01 11:32:30 +0200

    changed api

--- src/backend/api.py
+++ src/backend/api.py
@@ -14,6 +14,7 @@ app = Quart(__name__)
 @app.route("/api/cashkurs", methods=["GET", "POST"])
 async def cashkurs():
     if request.method == "POST":
+        print("called")
         body = await request.json
         print(body)
         if "challenge" in body:
